b18363c095d182c24723155b754803157bbadca4
athiththan11 <code19111995@gmail.com>
2020-04-15 13:30:51 +0530

    class typo fixes

--- src/components/Reference.js
+++ src/components/Reference.js
@@ -2,13 +2,11 @@ import React from 'react'
 
 const Reference = ({ title, description, hyperlink }) => {
   return (
-    <a class="reference-card ul-disabled" href={hyperlink}>
-      <div class="card">
-        <div class="card-horizontal">
-          <div class="card-body py-4 px-5">
-            <h4 class="card-title mt-4">
-              {title}
-            </h4>
+    <a className="reference-card ul-disabled" href={hyperlink}>
+      <div className="card">
+        <div className="card-horizontal">
+          <div className="card-body py-4 px-5">
+            <h4 className="card-title mt-4">{title}</h4>
             <p>{description}</p>
           </div>
         </div>
