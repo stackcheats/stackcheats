05d93ca5dfd52533ef233bff9eb768fe2ad7349d
athiththan11 <code19111995@gmail.com>
2020-04-22 23:09:17 +0530

    google analytics tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 module.exports = {
 	siteMetadata: {
 		title: `StackCheats`,
@@ -86,7 +88,7 @@ module.exports = {
 		{
 			resolve: `gatsby-plugin-google-analytics`,
 			options: {
-				//trackingId: `ADD YOUR TRACKING ID HERE`,
+				trackingId: process.env.GA_TRACKING_ID,
 			},
 		},
 		{
