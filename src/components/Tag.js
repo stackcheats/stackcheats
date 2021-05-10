6426b313a155c00a037532ecb0ebc6d1bf81d62e
athiththan11 <code19111995@gmail.com>
2021-05-10 22:15:34 +0530

    prettier

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -9,7 +9,11 @@ function Tag({ tags, ...props }) {
   return (
     <div {...props}>
       {tags.map(tag => (
-        <Link key={tag} className="badge badge-primary font-weight-normal" to={`/tags/${tag}/`}>
+        <Link
+          key={tag}
+          className="badge badge-primary font-weight-normal"
+          to={`/tags/${tag}/`}
+        >
           {tag}
         </Link>
       ))}
